f0a43d938a81f6fb77b6de50125e0525a6fd2370
absencee_ <ilyasofli06@gmail.com>
2025-01-23 15:08:33 +0300

    Update CreateDefaultDirectories.cs

--- Editor/CreateDefaultDirectories.cs
+++ Editor/CreateDefaultDirectories.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 
 namespace com.absence.utilities.editor
 {
+    /// <summary>
+    /// The static class responsible for creating the default directories (with the maximum depth of 2 layers).
+    /// </summary>
     public static class CreateDefaultDirectories
     {
         private static readonly string[] FOLDERS = {
@@ -26,6 +31,9 @@ namespace com.absence.utilities.editor
         "Textures/Sprites",
         };
 
+        public static event Action<List<string>> OnExtraFolderCreation = null;
+        public static event Action<List<string>> OnExtraSubfolderCreation = null;
+
         [MenuItem("absencee_/absent-utilities/Create Default Directories")]
         public static void CreateDirectories()
         {
@@ -35,10 +43,30 @@ namespace com.absence.utilities.editor
                 AssetDatabase.CreateFolder("Assets", FOLDERS[i]);
             }
 
+            List<string> extraFolders = new();
+            OnExtraFolderCreation?.Invoke(extraFolders);
+
+            for (int i = 0; i < extraFolders.Count; i++)
+            {
+                if (AssetDatabase.IsValidFolder(Path.Combine("Assets", extraFolders[i]))) continue;
+                AssetDatabase.CreateFolder("Assets", extraFolders[i]);
+            }
+
             for (int i = 0; i < SUBFOLDERS.Length; i++)
             {
                 if (AssetDatabase.IsValidFolder(Path.Combine("Assets", SUBFOLDERS[i]))) continue;
                 string[] seperatedFolderName = SUBFOLDERS[i].Split('/');
+                int depth = seperatedFolderName.Length;
+                AssetDatabase.CreateFolder(Path.Combine("Assets", seperatedFolderName[depth - 2]), seperatedFolderName[depth - 1]);
+            }
+
+            List<string> extraSubfolders = new();
+            OnExtraSubfolderCreation?.Invoke(extraSubfolders);
+
+            for (int i = 0; i < extraSubfolders.Count; i++)
+            {
+                if (AssetDatabase.IsValidFolder(Path.Combine("Assets", extraSubfolders[i]))) continue;
+                string[] seperatedFolderName = extraSubfolders[i].Split('/');
                 int depth = seperatedFolderName.Length;
                 AssetDatabase.CreateFolder(Path.Combine("Assets", seperatedFolderName[depth - 2]), seperatedFolderName[depth - 1]);
             }
